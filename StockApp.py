e391ac72cf79edd1aabcd04766f0f1bf6648cf93
Syed Hoque <srh33227@uga.edu>
2026-01-25 17:47:03 -0500

    added watchlist

--- StockApp.py
+++ StockApp.py
@@ -1,8 +1,10 @@
 import os
 import requests
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
+
 
 class StockApp:
     def __init__(self):
@@ -17,7 +19,8 @@ class StockApp:
 
         if (response.status_code == 200):
             data = response.json()
-            return data;
+            print(f"{symbol} retrieved.")
+            return data
         else:
             print(f"Failed to retrive {name} stock.")
 
@@ -35,12 +38,22 @@ class StockApp:
         print(f"High: ${stock_info["2. high"]}")
         print(f"Low: ${stock_info["3. low"]}")
         print(f"Close: ${stock_info["4. close"]}")
-        print(f"Volume: {stock_info["5. volume"]}")
+        print(f"Volume: {stock_info["5. volume"]}\n")
+
 
 if __name__ == "__main__":
     app = StockApp()
-    ticker = input("Enter a stock (ticker): ").capitalize()
+    file_path = 'watchlist.txt'
+    try:
+        with open(file_path, "r") as file:
+            tickers = file.read().splitlines()
 
-    stock_info = app.getStockInfo(ticker)
-    app.printStockInfo(ticker, stock_info);
+        for ticker in tickers:
+            stock_info = app.getStockInfo(ticker)
+            app.printStockInfo(ticker, stock_info)
+            time.sleep(2)
 
+    except FileNotFoundError:
+        print(f"Error: The file '{file_path} wasn't found.")
+
+    file.close()
